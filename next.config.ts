6d44e425e760a327f0cf66547f316222cfc0584c
Khosbileg <khosbileg@spikestudio.jp>
2026-03-02 11:16:43 +0900

    github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+const repo = process.env.GITHUB_REPOSITORY?.split("/")[1] || "";
+const isProd = process.env.NODE_ENV === "production";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+module.exports = {
+  output: "export",
+  // GitHub Pages дээр “folder” доор serve хийдэг тул:
+  basePath: isProd ? `/${repo}` : "",
+  assetPrefix: isProd ? `/${repo}/` : "",
+  trailingSlash: true,
+  // next/image ашиглаж байвал Pages дээр default image optimization ажиллахгүй тул:
+  images: { unoptimized: true },
 };
-
-export default nextConfig;
